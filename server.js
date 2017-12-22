4c0f38620e3dfcfc76f4ce1e709d40ad2c3183a0
Ubuntu <ubuntu@ip-172-31-19-210.ap-south-1.compute.internal>
2017-12-22 15:42:27 +0000

    server app 100

---
+++ server.js
@@ -0,0 +1,55 @@
+//import { unescape } from 'querystring';
+
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+
+//Array of mime Types
+var mimeTypes = {
+  "html" : "text/html",
+  "jpeg" : "image/jpeg",
+  "jpg" : "image/jpeg",
+  "png" : "image/png",
+  "javascript" : "text/javascript",
+  "css" : "text/css"
+};
+
+http.createServer(function (req, res) {
+  var uri = url.parse(req.url).pathname;
+  var fileName = path.join(process.cwd(),unescape(uri));
+  console.log('loading' + uri);
+  var stats;
+
+  try {
+          stats = fs.lstatSync(fileName);
+  } catch(e) {
+          res.writeHead(404,{'Content-type' : 'text/plain'});
+          res.write('404 Not Found\n');
+          res.end();
+          return;
+  }
+
+  if(stats.isFile()){
+    var mimeType = mimeTypes[path.extname(fileName.split(".").reverse()[0])];
+    res.writeHead(200,{'Content-type' : 'text/html'});
+
+    var fileStream =fs.createReadStream(fileName);
+    fileStream.pipe(res);
+  } else if(stats.isDirectory())  {
+    res.writeHead(302, {
+      'Location' : 'index.html'
+    });
+    res.end();
+  } else  {
+    res.writeHead(500, {'Content-type' : 'text/plain'});
+    res.write('500 Internal Error\n');
+    res.end();
+
+  }
+
+}).listen(3000);
+
+
+console.log('Server runnuing on localhost:3000');
