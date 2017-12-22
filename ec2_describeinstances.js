abb0f419c344dd8c033f55690516fc25de4a7123
Sunil <sunilnist340@gmail.com>
2017-12-22 08:15:07 +0530

    101

---
+++ ec2_describeinstances.js
@@ -0,0 +1,17 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'ap-south-1'});
+AWS.config.loadFromPath('./config.json');
+
+// Create EC2 service object
+ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+// Call EC2 to retrieve the policy for selected bucket
+ec2.describeInstances(params, function(err, data) {
+  if (err) {
+    console.log("Error", err.stack);
+  } else {
+    console.log("Success", JSON.stringify(data));
+  }
+});
---
+++ ec2_startstopinstances.js
@@ -0,0 +1,47 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'ap-south-1'});
+AWS.config.loadFromPath('./config.json');
+
+// Create EC2 service object
+ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+var params = {
+  InstanceIds: [process.argv[3]],
+  DryRun: true
+};
+
+if (process.argv[2].toUpperCase() === "START") {
+  // call EC2 to start the selected instances
+  ec2.startInstances(params, function(err, data) {
+    if (err && err.code === 'DryRunOperation') {
+      params.DryRun = false;
+      ec2.startInstances(params, function(err, data) {
+          if (err) {
+            console.log("Error", err);
+          } else if (data) {
+            console.log("Success", data.StartingInstances);
+          }
+      });
+    } else {
+      console.log("You don't have permission to start instances.");
+    }
+  });
+} else if (process.argv[2].toUpperCase() === "STOP") {
+  // call EC2 to stop the selected instances
+  ec2.stopInstances(params, function(err, data) {
+    if (err && err.code === 'DryRunOperation') {
+      params.DryRun = false;
+      ec2.stopInstances(params, function(err, data) {
+          if (err) {
+            console.log("Error", err);
+          } else if (data) {
+            console.log("Success", data.StoppingInstances);
+          }
+      });
+    } else {
+      console.log("You don't have permission to stop instances");
+    }
+  });
+}
