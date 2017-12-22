66004f55c9bef4d9d177d134b2893addc6c898c7
Ubuntu <ubuntu@ip-172-31-22-244.ap-south-1.compute.internal>
2017-12-22 02:30:54 +0000

    First commit from aws server

---
+++ ec2_createinstances.js
@@ -0,0 +1,35 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'ap-south-1'});
+AWS.config.loadFromPath('./config.json');
+
+// Create EC2 service object
+var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+var params = {
+   ImageId: 'ami-e41b618b', // Ubuntu Server 16.04 LTS
+   InstanceType: 't2.micro',
+   MinCount: 1,
+   MaxCount: 1
+};
+
+// Create the instance
+ec2.runInstances(params, function(err, data) {
+   if (err) {
+      console.log("Could not create instance", err);
+      return;
+   }
+   var instanceId = data.Instances[0].InstanceId;
+   console.log("Created instance", instanceId);
+   // Add tags to the instance
+   params = {Resources: [instanceId], Tags: [
+      {
+         Key: 'Name',
+         Value: 'SDK Sample'
+      }
+   ]};
+   ec2.createTags(params, function(err) {
+      console.log("Tagging instance", err ? "failure" : "success");
+   });
+});
